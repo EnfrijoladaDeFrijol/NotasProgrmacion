4448034d04333cc594eefc4aa92ccd15e1af1858
unknown <arturomezsanchez@gmail.com>
2024-01-01 15:52:34 -0600

    [FIX]

--- Python/Excel/usoExcel.py
+++ Python/Excel/usoExcel.py
@@ -4,6 +4,6 @@ import pandas as pd
 # Para acceder ala rchivo
 archivoExcel = pd.ExcelFile("/Users/Arthur/Documents/NotasProgramacion/Python/Excel/archivoEjemplo.xlsx")
 # Especificamos alhoja
-misDatos = archivoExcel.parse('Hoja 1')
+misDatos = archivoExcel.parse('Hoja1')
 
 print(misDatos)
